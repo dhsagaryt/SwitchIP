373b53730125a24bbb0ff7209fa5103a90890c19
Sagar Mondal <128616069+dhsagaryt@users.noreply.github.com>
2025-02-13 00:48:12 +0530

    Update SwitchIP 2.3.bat

--- SwitchIP 2.3.bat
+++ SwitchIP 2.3.bat
@@ -17,3 +17,29 @@ IF NOT "%~n0"=="%proName% %proVer%" (
     pause
     exit
 )
+
+set logfile=%USERPROFILE%\Desktop\SWITCHIP-[%date%].log
+echo Starting SwitchIP At Time [%date%] [%time%] >> %logfile%
+
+goto bnr
+pause
+
+:bnr
+echo.
+echo  [33m███████[0m╗[33m██[0m╗    [33m██[0m╗[33m██[0m╗[33m████████[0m╗ [33m██████[0m╗[33m██[0m╗  [33m██[0m╗[33m██[0m╗[33m██████[0m╗ 
+echo  [33m██[0m╔════╝[33m██[0m║    [33m██[0m║[33m██[0m║╚══[33m██[0m╔══╝[33m██[0m╔════╝[33m██[0m║  [33m██[0m║[33m██[0m║[33m██[0m╔══[33m██[0m╗
+echo  [33m███████[0m╗[33m██[0m║ [33m█[0m╗ [33m██[0m║[33m██[0m║   [33m██[0m║   [33m██[0m║     [33m███████[0m║[33m██[0m║[33m██████[0m╔╝
+echo  ╚════[33m██[0m║[33m██[0m║[33m███[0m╗[33m██[0m║[33m██[0m║   [33m██[0m║   [33m██[0m║     [33m██[0m╔══[33m██[0m║[33m██[0m║[33m██[0m╔═══╝ 
+echo  [33m███████[0m║╚[33m███[0m╔[33m███[0m╔╝[33m██[0m║   [33m██[0m║   ╚[33m██████[0m╗[33m██[0m║  [33m██[0m║[33m██[0m║[33m██[0m║ [92m%proVer%[0m
+echo  ╚══════╝ ╚══╝╚══╝ ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝╚═╝     
+echo.
+echo  [94mThis tool will attempt to fix common network issues by[0m
+echo  [94mresetting the IP configuration and clearing DNS.[0m
+echo.
+
+:OptMenu1
+CHOICE /N /C:YN /M "[96m   [0m Do You Really Want To SwitchIP? [[96mY[0m/[96mN[0m]:"%1
+:: Handle user input
+IF ERRORLEVEL ==2 echo. & GOTO Endxit
+IF ERRORLEVEL ==1 GOTO BasicOpt
+goto OptMenu1
